6837270a5f3676e5887880b35111cd080831f3c4
Clemen <clemente.rodriguezre@elorrieta-errekamari.com>
2023-11-16 10:42:28 +0100

    commit clase coordenadas

--- app/src/main/java/com/example/portugaletego/vista/MainActivity.java
+++ app/src/main/java/com/example/portugaletego/vista/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.portugaletego;
+package com.example.portugaletego.vista;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.portugaletego.R;
 
 public class MainActivity extends AppCompatActivity {
 
