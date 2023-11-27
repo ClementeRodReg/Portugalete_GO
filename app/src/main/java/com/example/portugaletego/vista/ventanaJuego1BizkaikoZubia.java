3d2417e44cb66b4969e461792516459ac6ca1a32
Ander <ander.delaiglesiape@elorrieta-errekamari.com>
2023-11-27 12:25:32 +0100

    juego 1.3

--- app/src/main/java/com/example/portugaletego/vista/ventanaJuego1BizkaikoZubia.java
+++ app/src/main/java/com/example/portugaletego/vista/ventanaJuego1BizkaikoZubia.java
@@ -2,15 +2,30 @@ package com.example.portugaletego.vista;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.portugaletego.R;
+import com.example.portugaletego.controlador.ControladorPasarVentana;
 
 public class ventanaJuego1BizkaikoZubia extends AppCompatActivity {
 
+    Button btnConfirmar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ventana_juego1_bizkaiko_zubia);
+        btnConfirmar = findViewById(R.id.btnConfirmar);
+
+        btnConfirmar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent (ventanaJuego1BizkaikoZubia.this, VistaMapa.class);
+                startActivity(i);
+            }
+        });
     }
 }
