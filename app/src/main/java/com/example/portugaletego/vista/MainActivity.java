a9a65b1dae77ffd6d57f183c86587f40e38be4f6
Clemen <clemente.rodriguezre@elorrieta-errekamari.com>
2023-11-29 08:21:07 +0100

    commit arreglar intro

--- app/src/main/java/com/example/portugaletego/vista/MainActivity.java
+++ app/src/main/java/com/example/portugaletego/vista/MainActivity.java
@@ -2,20 +2,24 @@ package com.example.portugaletego.vista;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.example.portugaletego.R;
 import com.example.portugaletego.controlador.ControladorPasarVentana;
+
+import pl.droidsonroids.gif.GifImageView;
 
 public class MainActivity extends AppCompatActivity {
     Intent mandar;
     final Handler handler = new Handler();
     final int delay = 500;
     int insertado = 0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
