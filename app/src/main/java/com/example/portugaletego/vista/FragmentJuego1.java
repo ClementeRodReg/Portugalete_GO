8d1f812ab9eb389c12a549651100236f8ea9a49d
Sergio Galera <122434581+UnaiCano@users.noreply.github.com>
2023-12-21 14:17:36 +0100

    Push pre-vacas

--- app/src/main/java/com/example/portugaletego/vista/FragmentJuego1.java
+++ app/src/main/java/com/example/portugaletego/vista/FragmentJuego1.java
@@ -5,57 +5,17 @@ import android.content.ClipDescription;
 import android.view.DragEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 public class FragmentJuego1 extends Fragment {
 
-    ImageView recogida;
+    ImageView imgPasarela, imgAscensor, imgTensor, imgTransbordador;
 
-    @Override
-    public boolean onLongClick(View v) {
+    TextView placeholder1, placeholder2, placeholder3, placeholder4;
 
-        recogida = (ImageView) v;
 
-        ClipData.Item item = new ClipData.Item((CharSequence) v.getTag());
 
-        ClipData dragData = new ClipData((CharSequence) v.getTag(),
-                new String[]{ClipDescription.MIMETYPE_TEXT_PLAIN},
-                item);
-
-        View.DragShadowBuilder myShadow = new View.DragShadowBuilder(v);
-
-        v.startDragAndDrop(dragData,myShadow,null,0);
-
-        return true;
-
-    }
-
-    View.OnDragListener drag = (v, event) -> {
-
-        int dragEvent = event.getAction();
-
-        switch (dragEvent) {
-
-            case DragEvent.ACTION_DROP:
-
-                if (v.getId() == R.id.idtv1 && recogida.getId() == R.id.idImagensiu) {
-
-                    v.setBackground(recogida.getDrawable());
-
-                    recogida.setVisibility(View.GONE);
-
-                    Toast toast1 = Toast.makeText(getApplicationContext(),"BIEN", Toast.LENGTH_SHORT);
-                    toast1.show();
-                } else {
-                    Toast toast1 = Toast.makeText(getApplicationContext(),"MAL", Toast.LENGTH_SHORT);
-                    toast1.show();
-                }
-
-        }
-
-        return true;
-
-    };
 }
