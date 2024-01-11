92195306e3241b6c0a307577e86681bca5d83808
Sergio Galera <122434581+UnaiCano@users.noreply.github.com>
2024-01-11 11:47:47 +0100

    fragmento juego2 completo
    falta corregir las puntuaciones

---
+++ app/src/main/java/com/example/portugaletego/modelo/Respuestas.java
@@ -0,0 +1,38 @@
+package com.example.portugaletego.modelo;
+
+public class Respuestas {
+
+    public static String question[] ={
+            "Zer da patroia?",
+            "Zenbat arraunlari daude trineruan?",
+            "Nor irabazi du 2023 liga?",
+            "Nola dauka izena Portugaleteko taldea?",
+            "Traineruekin zer arrantzazen zire?",
+            "Noiz hasi zen industrializazioa Espainian?",
+            "Zer hiri ez da Ezkerraldekoa?",
+            "Zer industria zen nagusia ezkerraldean?"
+    };
+
+    public static String choices[][] = {
+            {"Uretatik kanpo argibideak ematen siguen pertsona.","Traineruaren atzekaldean dagoen arraunlaria, hau zuzentzen duena.","Traineruaren ondoan beste txalupa batean doan pertsona","Arraunlaririk zaharrena."},
+            {"11","16","14","13"},
+            {"Urdaibai","Sotera","Orio","Zierbena"},
+            {"Nikolas Deuna","Portugaletetarrak","Jarrillera","Luffy"},
+            {"Baleak","Sardinak","Legatzak","Bakailoa"},
+            {"1790-1800","1850-1900","1800-1850","1900+"},
+            {"Trapagaran ","Barakaldo","Sestao","Bilbao"},
+            {"Arrantza","Ontziolak","Siderurgia","Merkataritza"}
+    };
+
+    public static String correctAnswers[] = {
+            "Traineruaren atzekaldean dagoen arraunlaria, hau zuzentzen duena.",
+            "13",
+            "Urdaibai",
+            "Jarrillera",
+            "Baleak",
+            "1800-1850",
+            "Trapagaran",
+            "Siderurgia"
+    };
+
+}
--- app/src/main/java/com/example/portugaletego/vista/FragmentJuego2.java
+++ app/src/main/java/com/example/portugaletego/vista/FragmentJuego2.java
@@ -2,6 +2,8 @@ package com.example.portugaletego.vista;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,6 +11,17 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.portugaletego.R;
+import com.example.portugaletego.modelo.Respuestas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +38,17 @@ public class FragmentJuego2 extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    TextView totalQuestionsTextView;
+    TextView questionTextView;
+    Button ansA, ansB, ansC, ansD;
+    Button submitBtn;
+
+    int score=0;
+    int totalQuestion = Respuestas.question.length;
+    int currentQuestionIndex = 0;
+    String selectedAnswer = "";
+    private View vista;
 
     public FragmentJuego2() {
         // Required empty public constructor
@@ -60,7 +84,94 @@ public class FragmentJuego2 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_juego2, container, false);
+        vista= inflater.inflate(R.layout.fragment_juego2, container, false);
+        return vista;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstance) {
+        super.onViewCreated(view, savedInstance);
+
+        totalQuestionsTextView = view.findViewById(R.id.total_question);
+        questionTextView = view.findViewById(R.id.question);
+        ansA = view.findViewById(R.id.ans_A);
+        ansB = view.findViewById(R.id.ans_B);
+        ansC = view.findViewById(R.id.ans_C);
+        ansD = view.findViewById(R.id.ans_D);
+        submitBtn = view.findViewById(R.id.submit_btn);
+
+        ansA.setOnClickListener(this::onClick);
+        ansB.setOnClickListener(this::onClick);
+        ansC.setOnClickListener(this::onClick);
+        ansD.setOnClickListener(this::onClick);
+        submitBtn.setOnClickListener(this::onClick);
+
+        totalQuestionsTextView.setText("Preguntas totales : "+totalQuestion);
+
+        loadNewQuestion();
+    }
+
+
+    public void onClick(View view) {
+
+
+        ansA.setBackgroundColor(Color.WHITE);
+        ansB.setBackgroundColor(Color.WHITE);
+        ansC.setBackgroundColor(Color.WHITE);
+        ansD.setBackgroundColor(Color.WHITE);
+
+        Button clickedButton = (Button) view;
+        if(clickedButton.getId()==R.id.submit_btn){
+            if(selectedAnswer.equals(Respuestas.correctAnswers[currentQuestionIndex])){
+                score++;
+            }
+            currentQuestionIndex++;
+            loadNewQuestion();
+
+
+        }else{
+            //choices button clicked
+            selectedAnswer  = clickedButton.getText().toString();
+            clickedButton.setBackgroundColor(Color.MAGENTA);
+
+        }
+
+    }
+
+    void loadNewQuestion(){
+
+        if(currentQuestionIndex == totalQuestion ){
+            finishQuiz();
+            return;
+        }
+
+        questionTextView.setText(Respuestas.question[currentQuestionIndex]);
+        ansA.setText(Respuestas.choices[currentQuestionIndex][0]);
+        ansB.setText(Respuestas.choices[currentQuestionIndex][1]);
+        ansC.setText(Respuestas.choices[currentQuestionIndex][2]);
+        ansD.setText(Respuestas.choices[currentQuestionIndex][3]);
+
+    }
+
+    void finishQuiz(){
+        String passStatus = "";
+        if(score > totalQuestion*0.60){
+            passStatus = "Passed";
+        }else{
+            passStatus = "Failed";
+        }
+
+        new AlertDialog.Builder(getContext())
+                .setTitle(passStatus)
+                .setMessage("Puntuación "+ score+" de "+ totalQuestion)
+                .setPositiveButton("Reiniciar",(dialogInterface, i) -> restartQuiz() )
+                .setCancelable(false)
+                .show();
+    }
+
+    void restartQuiz(){
+        score = 0;
+        currentQuestionIndex =0;
+        loadNewQuestion();
     }
 }
