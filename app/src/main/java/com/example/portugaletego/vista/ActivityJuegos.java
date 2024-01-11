6a215972b801625f7235b977943a3063a33c1445
Sergio Galera <122434581+UnaiCano@users.noreply.github.com>
2024-01-11 09:15:52 +0100

    push activity juegos

--- app/src/main/java/com/example/portugaletego/vista/ActivityJuegos.java
+++ app/src/main/java/com/example/portugaletego/vista/ActivityJuegos.java
@@ -5,20 +5,53 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.portugaletego.R;
 
 public class ActivityJuegos extends AppCompatActivity {
 
-    Button btnConfirmar;
+    Button btnVolver, btnSiguiente;
+    ImageButton btnMute, btnUnmute;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juegos);
+
+        btnVolver = findViewById(R.id.btnvolver);
+        btnSiguiente = findViewById(R.id.btnSiguiente);
+        btnMute = findViewById(R.id.btnMute);
+        btnUnmute = findViewById(R.id.btnUnmute);
+
+        AudioManager amanager=(AudioManager)getSystemService(Context.AUDIO_SERVICE);
+
+        btnVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                volver();
+            }
+        });
+
+        btnMute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mutear(amanager);
+            }
+        });
+
+        btnUnmute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                unmute(amanager);
+            }
+        });
 
         int id = getIntent().getIntExtra("id", 0);
 
@@ -27,25 +60,46 @@ public class ActivityJuegos extends AppCompatActivity {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-        Fragment fragment = fragmentManager.findFragmentById(R.id.fragmentJuego1);
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragmentJuegos);
 
         //Tendremos que meterlo dentro de un intent al seleccionar el pulsador en el mapa
        if(id == 1) {
            Fragment nuevoFragment = new FragmentJuego1();
-           fragmentTransaction.replace(R.id.fragmentJuego1, nuevoFragment);
+           fragmentTransaction.replace(R.id.fragmentJuegos, nuevoFragment);
        }
        else if(id == 2){
           Fragment nuevoFragment = new FragmentJuego2();
-          fragmentTransaction.replace(R.id.fragmentJuego2, nuevoFragment);
+          fragmentTransaction.replace(R.id.fragmentJuegos, nuevoFragment);
        }
        else if(id == 3){
-           Fragment nuevoFragment = new FragmentJuego3();
-            fragmentTransaction.replace(R.id.fragmentJuego3, nuevoFragment);
+           Fragment nuevoFragment = new fragmentCamara();
+            fragmentTransaction.replace(R.id.fragmentJuegos, nuevoFragment);
        }
        else if(id == 4){
-            Fragment nuevoFragment = new FragmentJuego4();
-            fragmentTransaction.replace(R.id.fragmentJuego4, nuevoFragment);
+            Fragment nuevoFragment = new fragmentCamara();
+            fragmentTransaction.replace(R.id.fragmentJuegos, nuevoFragment);
        }
         fragmentTransaction.commit();
     }
+
+
+    public void volver(){
+        Intent mandar = new Intent(this, VistaMapa.class);
+        startActivity(mandar);
+    }
+
+    public void mutear(AudioManager amanager){
+
+        amanager.adjustVolume(AudioManager.ADJUST_MUTE, AudioManager.FLAG_SHOW_UI);
+        btnMute.setVisibility(View.INVISIBLE);
+        btnUnmute.setVisibility(View.VISIBLE);
+    }
+
+    public void unmute(AudioManager amanager){
+
+        amanager.adjustVolume(AudioManager.ADJUST_UNMUTE, AudioManager.FLAG_SHOW_UI);
+        btnUnmute.setVisibility(View.INVISIBLE);
+        btnMute.setVisibility(View.VISIBLE);
+    }
+
 }
