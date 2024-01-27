3c5367fd18115846f8a85161a29b35cc682621a0
UnaiCM <122434581+UnaiCano@users.noreply.github.com>
2024-01-27 17:49:08 +0100

    Commit 27 Sabado tarde 1
    18 archivos cambiados
    -Aprendido como funcionan los styles
    -cambiado estetico de la mayoria
    -ARREGLADO JUEGO 2 los botones se colorean y se ve la seleccion
     (A veces dice que las respuestas no son correctas, hay que revisarlo)
    -Falta hacer una imagen para los 4 conectores en el Juego 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,12 +40,13 @@ dependencies {
     val roomversion = "2.5.0"
 
     implementation("org.osmdroid:osmdroid-android:6.1.17")
-
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.room:room-runtime:$roomversion")
     annotationProcessor("androidx.room:room-compiler:$roomversion")
     implementation("pl.droidsonroids.gif:android-gif-drawable:1.2.22")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.0.0")
+    implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("androidx.annotation:annotation:1.7.0")
